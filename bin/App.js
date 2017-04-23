b3eb694f7c4c37b43a2c821b8c744e96b3f75fd8
Sviat Wellnine <wellninedotnet@gmail.com>
2017-04-23 12:57:03 +0300

    - separated modules

--- bin/App.js
+++ bin/App.js
@@ -1,63 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
 import {
-  AppRegistry,
-  Text,
-  Button,
-  View,
-  TextInput,
-  Slider,
-  Picker,
-  Switch
+  AppRegistry
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import Form from 'react-native-form'
+//import Form from 'react-native-form'
 
 
+import { SignInComponent } from "./modules/auth";
 import HomeModule from "./modules/home/Home.module";
-
-export default class EntryModule extends Component {
-
-  static navigationOptions = {
-    title: 'Entry screen 1',
-  };
-
-  render() {
-    const { navigate } = this.props.navigation;
-
-    return (
-      <View>
-        <Text>Hello,Maelstorm. Music genre randomizer!</Text>
-        <Form ref="form">
-          <TextInput  placeholder="Login" type="TextInput" name="login" />
-          <TextInput  secureTextEntry placeholder="Password" type="TextInput" name="password" />
-
-          <Button
-            onPress={() => navigate('Home')}
-            title="Sign In"
-          />
-
-          <Switch type="Switch" name="mySwitch" />
-          <Slider type="Slider" name="mySlider" />
-          <Picker type="Picker" name="myPicker" />
-
-        </Form>
-
-      </View>
-    );
-
-  }
-}
 
 
 const maelstorm = StackNavigator({
-  Entry: { screen: EntryModule },
+  Entry: { screen: SignInComponent },
   Home: { screen : HomeModule }
 });
 
---
+++ bin/modules/auth/components/SignIn.component.js
@@ -0,0 +1,56 @@
+import React, { Component, PropTypes } from 'react';
+
+import {
+  AppRegistry,
+  Text,
+  Button,
+  View,
+  TextInput,
+  Slider,
+  Picker,
+  Switch
+} from 'react-native';
+
+import Form from 'react-native-form';
+
+
+export default class SignInComponent extends Component {
+
+  static navigationOptions = {
+    title: 'Entry screen 1',
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View>
+        <Text>Hello,Maelstorm. Music genre randomizer!</Text>
+        <Form ref="form">
+          <TextInput placeholder="Login" type="TextInput" name="login"/>
+          <TextInput secureTextEntry placeholder="Password" type="TextInput" name="password"/>
+
+          <Button
+            onPress={() => navigate('Home')}
+            title="Sign In"
+          />
+
+          <Switch type="Switch" name="mySwitch"/>
+          <Slider type="Slider" name="mySlider"/>
+          <Picker type="Picker" name="myPicker"/>
+
+        </Form>
+
+      </View>
+    );
+
+  }
+}
+
+/**
+ *
+ * @type {{navigate: function}} -> s react stack navigator action ( move to )
+ */
+SignInComponent.propTypes = {
+  navigate : PropTypes.func
+};
