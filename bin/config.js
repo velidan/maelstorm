2562d86476a24730dcb1911806aa7acce99ab57a
Sviat Wellnine <wellninedotnet@gmail.com>
2017-04-23 17:02:28 +0300

    added:  - config / debug / genre picker

---
+++ bin/config.js
@@ -0,0 +1,13 @@
+export default {
+  appApiKey : "KUC6IU4SGXXY11AP"
+}
+
+//https://freemusicarchive.org/api/get/tracks.json?api_key=KUC6IU4SGXXY11AP&genre_id=12
+
+//https://freemusicarchive.org/api/get/genres.json?api_key=KUC6IU4SGXXY11AP
+
+//genre rock id = 12
+
+// track file  music/Music_for_Video/Mscaras/Mscara_vs_Mscara/Mscaras_-_05_-_NewYorican.mp3
+
+// full music file url - https://files.freemusicarchive.org/music/Music_for_Video/Mscaras/Mscara_vs_Mscara/Mscaras_-_05_-_NewYorican.mp3
--- bin/modules/home/Home.module.js
+++ bin/modules/home/Home.module.js
@@ -1,7 +1,44 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Picker } from "react-native";
+
+
+const Item = Picker.Item;
+
+const genres = {
+  rock : {
+    label : "Rock",
+    value : "12"
+  },
+  electronic : {
+    label : "Electronic",
+    value : "15"
+  },
+  country : {
+    label : "Country",
+    value : "9"
+  },
+};
+
 
 export default class HomeModule extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      defaultGenre : {
+        label : "Rock",
+        value : "12"
+      }
+    }
+
+  }
+
+  _onGenreSelect = genreId => {
+    console.log("genreId selected => ", genreId);
+    this.setState({ ...this.state, defaultGenre : { ...this.state.defaultGenre, value : genreId } })
+  };
+
   static navigationOptions = {
     title : "Home Module"
   };
@@ -10,6 +47,16 @@ export default class HomeModule extends Component {
     return (
       <View>
         <Text>Home Module content</Text>
+
+
+        <Picker
+          selectedValue={this.state.defaultGenre.value}
+          onValueChange={this._onGenreSelect}>
+          <Picker.Item label={ genres.rock.label } value={ genres.rock.value } />
+          <Picker.Item label={ genres.country.label } value={ genres.country.value } />
+          <Picker.Item label={ genres.electronic.label } value={ genres.electronic.value } />
+        </Picker>
+
       </View>
     )
   }
